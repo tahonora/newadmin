cd7453d96363de9c80e2e525010d006291e9d00f
Tiago Honorato <tahonora@gmail.com>
2022-07-11 14:59:34 -0300

    feat: add theme light and dark

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { ThemeProvider } from "@emotion/react";
 import { BrowserRouter } from "react-router-dom"
 import { AppRoutes } from "./routes";
+import { LighTheme } from "./shared/themes";
 
 export const App = () => {
   return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>    
+    <ThemeProvider theme={LighTheme}>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>    
+    </ThemeProvider>
   );
 }
---
+++ src/shared/themes/Light.ts
@@ -0,0 +1,23 @@
+import { createTheme } from "@mui/material"
+import { purple, red } from "@mui/material/colors"
+
+export const LighTheme = createTheme({
+  palette: {
+    primary: {
+      main: red[700],
+      dark: red[800],
+      light: red[500],
+      contrastText: '#ffffff'
+    },
+    secondary: {
+      main: purple[500], 
+      dark: purple[400],
+      light: purple[300],
+      contrastText: '#ffffff'
+    },
+    background: {
+      default: '#ffffff',
+      paper: '#f7f6f3'
+    }
+  }
+})
