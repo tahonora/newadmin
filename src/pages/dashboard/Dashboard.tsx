05b78f0982d84ab1866c561dca15b05149515cb4
Tiago Honorato <tahonora@gmail.com>
2022-07-12 19:14:04 -0300

    feat: Button and Search Toolbar Configuration

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,8 +1,16 @@
+import { BarraDeFerramentas } from '../../shared/components'
 import { LayoutBaseDePagina } from '../../shared/layouts'
 
 export const Dashboard = () => {
 
   return (
-    <LayoutBaseDePagina titulo='Dashboard' barraDeFerramentas={<>BarraDeFerramentas</>} >Testando</LayoutBaseDePagina>
+    <LayoutBaseDePagina 
+      titulo='Dashboard' 
+      barraDeFerramentas={(
+        <BarraDeFerramentas 
+          mostrarInputBusca
+        />
+      )} 
+    >Testando</LayoutBaseDePagina>
   )
 }
---
+++ src/shared/components/barra-de-ferramentas/BarraDeFerramentas.tsx
@@ -0,0 +1,61 @@
+import { Box, Button, Icon, Paper, TextField, useTheme } from '@mui/material'
+
+interface IBarraDeFerramentasProps {
+  textodeBusca?: string
+  mostrarInputBusca?: boolean
+  aoMudarTextoDeBusca?: (novoTexto: string) => void
+
+  textoBotaoNovo?: string
+  mostrarBotaoNovo?: boolean
+  aoClicarEmNovo?: () => void
+
+}
+
+export const BarraDeFerramentas: React.FC<IBarraDeFerramentasProps> = ({
+  textodeBusca = '', 
+  mostrarInputBusca = false, 
+  aoMudarTextoDeBusca,
+  textoBotaoNovo = 'Novo',
+  mostrarBotaoNovo = true,
+  aoClicarEmNovo,
+
+}) => {
+
+  const theme = useTheme()
+
+  return (
+    <Box 
+      marginX={1} 
+      padding={1} 
+      paddingX={2} 
+      display='flex' 
+      gap={1}
+      alignItems='center'
+      height={theme.spacing(5)} 
+      component={Paper} 
+    >
+      { mostrarInputBusca && (
+        <TextField
+          size='small'
+          value={textodeBusca}
+          onChange={(e) => aoMudarTextoDeBusca?.(e.target.value)}
+          placeholder='Pesquisar...'
+        />
+
+      )}
+      <Box flex={1} display='flex' justifyContent='end' >
+        {mostrarBotaoNovo && (
+          <Button
+            color='primary'
+            disableElevation
+            variant='contained'
+            onClick={aoClicarEmNovo}
+            endIcon={<Icon>add</Icon>}
+          >{textoBotaoNovo}</Button>
+        )}
+      </Box>
+      
+    </Box>
+  )
+
+}
