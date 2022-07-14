b286c1cd87cd3fffb500df2c92a7102e665b8cce
Tiago Honorato <tahonora@gmail.com>
2022-07-14 13:48:39 -0300

    feat: Terminating backend access services

--- src/shared/services/api/pessoas/PessoasService.ts
+++ src/shared/services/api/pessoas/PessoasService.ts
@@ -7,6 +7,7 @@ interface IListagemPessoa {
   cidadeId: number
   nomecompleto: string  
 }
+
 interface IDetalhePessoa {
   id: number
   email: string
@@ -37,13 +38,45 @@ const getAll = async (page = 1, filter = ''): Promise<PessoasComTotalCount | Err
   }
 }
 
-const getById = async (): Promise<any> => {}
+const getById = async (id: number): Promise<IDetalhePessoa | Error> => {
+  try {
+    const { data } = await Api.get(`pessoas/${id}`)
+    if (data) {
+      return data
+    }
+    return new Error('Erro ao consultar os registros.')
+  } catch (error) { 
+    return new Error( (error as {message: string}).message || 'Erro ao consultar os registros.' )
+  }
+}
 
-const create = async (): Promise<any> => {}
+const create = async (dados: Omit<IDetalhePessoa, 'id'>): Promise<number | Error> => {  
+  try {
+    const { data } = await Api.post<IDetalhePessoa>('/pessoas', dados)
+    if (data) {
+      return data.id
+    }
+    return new Error('Erro ao cadastrar o registro.')
+  } catch (error) { 
+    return new Error( (error as {message: string}).message || 'Erro ao cadastrar o registro.' )
+  }
+}
 
-const updateById = async (): Promise<any> => {}
+const updateById = async (id: number, dados: IDetalhePessoa): Promise<void | Error> => {
+  try {
+    await Api.put(`/pessoas/${id}`, dados)
+  } catch (error) { 
+    return new Error( (error as {message: string}).message || 'Erro ao atualizar o registro.' )
+  }
+}
 
-const deleteById = async (): Promise<any> => {}
+const deleteById = async (id: number): Promise<void | Error> => {
+  try {
+    await Api.delete(`/pessoas/${id}`)
+  } catch (error) { 
+    return new Error( (error as {message: string}).message || 'Erro ao apagar o registro.' )
+  }
+}
 
 export const PessoasService = {
   getAll,
