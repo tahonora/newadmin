a7fbaccbe2907197b2299a3d4f14c2cec5aeda3b
Tiago Honorato <tahonora@gmail.com>
2022-07-27 21:46:51 -0300

    update

--- server.js
+++ server.js
@@ -1,3 +1,20 @@
 
-console.log('batata')
+import express from 'express'
+import { resolve } from 'path'
+const app = express()
 
+app.use('/',
+  express.static(
+    resolve(
+      // eslint-disable-next-line no-undef
+      __dirname,
+      './build',
+    ),
+  ),
+)
+
+app 
+  .listen(3000, (err) => {
+    if (err) { return console.log(err)}
+    console.log('funcionando certinho')
+  })
